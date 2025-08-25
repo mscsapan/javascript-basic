3d3f2887b0f9287b833fb7e9d1d2f03977e1a796
Mohammad Ali <mscsapan@gmail.com>
2025-08-25 12:09:17 +0600

    📌 Optimized events functions with query selector

--- js/events.js
+++ js/events.js
@@ -1,19 +1,51 @@
+// Tag names ("p", "div")
+// Class names (".my-class")
+// IDs ("#my-id")
+// Attribute selectors ( "[type='text']")
+// Combinators ( ".parent > .child")
 
-const bodyText = document.getElementById('body');
 
 
-const buttonClick = document.getElementById('click-me');
+const fullBody = document.getElementById('full-body');
+
+const bodyText = document.querySelector('h1');
+
+const paragraphText = document.querySelector('p');
 
 
-buttonClick.addEventListener('mouseover', () => {
-    console.log('mouseover event working..');
+// const buttonClick = document.getElementById('click-me');
+
+
+// buttonClick.addEventListener('mouseover', () => {
+//     console.log('mouseover event working..');
+//     bodyText.innerHTML = 'Mouse is over the Button';
+//     //alert('Button is clicked');
+// });
+
+// buttonClick.addEventListener('mouseout', () => {
+//     console.log('mouseout event working..');
+//     bodyText.innerHTML = 'Mouse is out of the Button';
+//     //alert('Button is clicked');
+// });
+
+function addEvent(selector, event, handler) {
+    document.querySelector(selector).addEventListener(event, handler);
+}
+
+
+addEvent('#click-me', 'mouseover', function () {
+    // console.log('full-body');
     bodyText.innerHTML = 'Mouse is over the Button';
-    //alert('Button is clicked');
+    fullBody.style.backgroundColor = 'green';
 });
 
-buttonClick.addEventListener('mouseout', () => {
-    console.log('mouseout event working..');
-    bodyText.innerHTML = 'Mouse is out of the Button';
-    //alert('Button is clicked');
+addEvent('#click-me', 'mouseout', function () {
+    // console.log('full-body');
+    bodyText.innerHTML = 'Mouse is out the Button';
+    fullBody.style.backgroundColor = 'yellow';
+});
+
+addEvent('#full-body', 'scroll', function () {
+    console.log('scrolling pressing');
 });
 
