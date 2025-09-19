8fb9733c1e890cb40e4fe5590f780e68dc22be26
Mohammad Ali <mscsapan@gmail.com>
2025-09-19 20:56:56 +0600

    🏷️ Getter and setter function added but not work yet

--- js/oop/inheritance.js
+++ js/oop/inheritance.js
@@ -12,6 +12,26 @@ class Monitor extends Device {
     }
 }
 
-const monitor = new Monitor('Electric Monitor', 2020, 'device');
+// const monitor = new Monitor('Electric Monitor', 2020, 'device');
 
-console.log(monitor.type);
+// console.log(monitor.type);
+
+
+// getter and setter 
+
+
+class Car {
+    constructor(name) {
+        this.name = name;
+    }
+
+    get get_car_name() {
+        return this.name;
+    }
+
+    set set_car_name(name) {
+        this.name = name;
+    }
+}
+
+
