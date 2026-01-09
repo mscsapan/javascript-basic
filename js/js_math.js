ded7e05a4bca0ba85eb46a37321a991fe880feb3
Mohammad Ali <mscsapan@gmail.com>
2026-01-09 20:42:18 +0600

    ⚰️ More math function added

--- js/js_math.js
+++ js/js_math.js
@@ -1,3 +1,5 @@
-const num = 10.9960920192121;
+const num = 10.2960920192121;
 
-console.log(num.toFixed(3));
+// console.log(num.toFixed(3));
+// console.log(Math.round(num));
+console.log(Math.ceil(num));
