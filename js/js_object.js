0d91e4cc44becb656de886cd78337914de42e8c9
Mohammad Ali <mscsapan@gmail.com>
2026-01-13 18:24:45 +0600

    🔥 Object file code commented

--- js/js_object.js
+++ js/js_object.js
@@ -1,21 +1,23 @@
-const names = {
-    firstName: 'Mohammad',
-    lastName: 'Ali',
-    age: 24,
-    info: function () {
-        return `Your full name is ${this.firstName} ${this.lastName} and your age is ${this.age}`
-    },
-}
+// const names = {
+//     firstName: 'Mohammad',
+//     lastName: 'Ali',
+//     age: 24,
+//     info: function () {
+//         return `Your full name is ${this.firstName} ${this.lastName} and your age is ${this.age}`
+//     },
+// }
 
 
-const myList = ['Mohammad', 'Ali', 24, false, names];
+// const myList = ['Mohammad', 'Ali', 24, false, names];
 
 // console.log(names.firstName);
 // for (let i = 0; i < myList.length; i++) {
 //     console.log(myList[i]);
 // }
 
-const n = Object.entries(names);
+// const n = Object.entries(names);
+// const n = Object.keys(names);
+// const n = Object.values(names);
 
-console.log(names.info());
+// console.log(names.info());
 
