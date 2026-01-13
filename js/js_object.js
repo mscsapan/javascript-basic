6d6bd10db0c34887053527117163b41e670057e8
Mohammad Ali <mscsapan@gmail.com>
2026-01-13 18:23:14 +0600

    📝 Function added in object

--- js/js_object.js
+++ js/js_object.js
@@ -1,10 +1,21 @@
 const names = {
     firstName: 'Mohammad',
     lastName: 'Ali',
-    age: 24
+    age: 24,
+    info: function () {
+        return `Your full name is ${this.firstName} ${this.lastName} and your age is ${this.age}`
+    },
 }
+
 
 const myList = ['Mohammad', 'Ali', 24, false, names];
 
 // console.log(names.firstName);
-console.log(myList);
+// for (let i = 0; i < myList.length; i++) {
+//     console.log(myList[i]);
+// }
+
+const n = Object.entries(names);
+
+console.log(names.info());
+
