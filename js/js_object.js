b0993a07d8eb28a69f19fcdf09c931477f88cf86
Mohammad Ali <mscsapan@gmail.com>
2026-01-13 18:47:04 +0600

    ✅ New car model object added

--- js/js_object.js
+++ js/js_object.js
@@ -21,3 +21,55 @@
 
 // console.log(names.info());
 
+// const carModel = [
+//     {
+//         "name": "Pontiac",
+//         "model": "GTO",
+//         "year": 1972,
+//         "color": "Green",
+//         "country": "Poland"
+//     },
+//     {
+//         "name": "Dodge",
+//         "model": "Avenger",
+//         "year": 2000,
+//         "color": "Maroon",
+//         "country": "Uruguay"
+//     },
+//     {
+//         "name": "Dodge",
+//         "model": "Caliber",
+//         "year": 2009,
+//         "color": "Maroon",
+//         "country": "China"
+//     },
+//     {
+//         "name": "Jeep",
+//         "model": "Wrangler",
+//         "year": 1995,
+//         "color": "Turquoise",
+//         "country": "France"
+//     },
+//     {
+//         "name": "Lexus",
+//         "model": "IS",
+//         "year": 2012,
+//         "color": "Blue",
+//         "country": "Laos"
+//     },
+// ]
+
+
+const carModel = {
+    name: "Pontiac",
+    model: "GTO",
+    year: 1972,
+    color: ['Green', 'Red', 'Yello', 'White', 'Black'],
+    country: "Poland",
+    isAvailableNow: true,
+    detail: function () {
+        return `Car name is ${this.name}`;
+    },
+}
+
+console.log(carModel.detail());
