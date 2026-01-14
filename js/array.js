70c3fd8e3b31a8b7662f2964b2ad1f5f6a0d178b
Mohammad Ali <mscsapan@gmail.com>
2026-01-14 19:04:29 +0600

    ✅ Spread and Object added into js array

--- js/array.js
+++ js/array.js
@@ -1,3 +1,9 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const names = { firstName: 'Mohammad', lastName: 'Ali', age: 28 };
+
+const fruits2 = ['Water melon', 'Pine apple'];
+
+const fruits = ["Banana", "Orange", "Apple", "Mango", ...fruits2, names];
+
+fruits.push('Grapes')
 
 console.log(fruits);
