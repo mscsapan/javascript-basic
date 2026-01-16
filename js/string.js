83d3c6390d88c3cca0e0bc120635dac383b6c5c7
Mohammad Ali <mscsapan@gmail.com>
2026-01-16 18:49:33 +0600

    ♻️ JS Strng file modified

--- js/string.js
+++ js/string.js
@@ -3,10 +3,10 @@ const doubleQuotetion = "This is double quotetion text </br>";
 const templateText = `This is single templates text </br>`;
 const mixedText = `'Mohammad Ali', "Dhaka, Bangladesh"`;
 
-document.writeln(singleQuotetion);
-document.writeln(doubleQuotetion);
-document.writeln(templateText);
-document.writeln(mixedText);
+// document.writeln(singleQuotetion);
+// document.writeln(doubleQuotetion);
+// document.writeln(templateText);
+// document.writeln(mixedText);
 
 // const x = new String('Dhaka');
 // const y = new String('Dhaka');
@@ -17,3 +17,13 @@ document.writeln(mixedText);
 // } else {
 //     console.log('both aren\'t equal');
 // }
+
+
+const firstName = 'Mohammad';
+const lastName = ' Ali';
+
+// console.log(firstName + lastName);
+// console.log(firstName.at(2));//h
+// console.log(firstName.charAt(0));//h
+// console.log(firstName.concat(lastName));//Mohammad Ali
+// console.log(firstName.length);//8
