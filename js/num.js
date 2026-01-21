b6cea01a40945741256e6a37765305e3e3d123a1
Mohammad Ali <mscsapan@gmail.com>
2026-01-21 19:07:01 +0600

    ✅ JS Number file updated with number method

--- js/num.js
+++ js/num.js
@@ -11,3 +11,23 @@ const y = 0.4
 
 console.log(typeof convertedNum);
 console.log(Number(result) + x + y);
+
+const n = 248;
+
+const nToString = n.toString(2);//return bynamary fo 10 (1010)
+
+// console.log(`n type : ${typeof n} - nToString type ${typeof nToString}`);
+
+// console.log(nToString);
+
+let nMethod = Number.EPSILON;
+nMethod = Number.MAX_SAFE_INTEGER;
+nMethod = Number.MIN_SAFE_INTEGER;
+nMethod = Number.MAX_VALUE;
+nMethod = Number.MIN_VALUE;
+nMethod = Number.POSITIVE_INFINITY;
+nMethod = Number.NEGATIVE_INFINITY;
+nMethod = Number.NaN;
+nMethod = Number.isInteger;
+
+console.log(nMethod);
