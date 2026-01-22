4bedf6d742109c3376371a666fc139db366b1030
Mohammad Ali <mscsapan@gmail.com>
2026-01-22 19:06:50 +0600

    📝 JS Set object added

---
+++ js/ds/sets.js
@@ -0,0 +1,13 @@
+const myList = [1, 1, 2, 2, 3, 3, 4, 3, 5, 10, 10];
+const mySets = new Set();
+
+mySets.add('A')
+mySets.add('b')
+mySets.add('c')
+mySets.add('D')
+mySets.add('E')
+mySets.add('f')
+mySets.add('G')
+mySets.add('a')
+console.log(myList);
+console.log(new Set(myList));
