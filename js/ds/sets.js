8fc41a84fbf03260cb7aabbf3791455161d8bff9
Mohammad Ali <mscsapan@gmail.com>
2026-01-22 20:18:00 +0600

    ✅ Update JS Set file and added more method

--- js/ds/sets.js
+++ js/ds/sets.js
@@ -9,5 +9,23 @@ mySets.add('E')
 mySets.add('f')
 mySets.add('G')
 mySets.add('a')
-console.log(myList);
-console.log(new Set(myList));
+
+// console.log(myList);
+// console.log(new Set(myList));
+
+// mySets.keys().forEach((v, k) => console.log(v, k));
+
+// console.log(mySets.size);
+// console.log(mySets.keys());
+
+// for (const x of mySets.keys()) {
+//     console.log(x);
+// }
+// for (const x of mySets.values()) {
+//     console.log(x);
+// }
+
+// console.log(mySets.has('A'));
+// mySets.clear();
+// console.log(mySets.size);
+// console.log(mySets.delete('a'));
