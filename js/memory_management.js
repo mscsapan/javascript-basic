c2d4494e778dd90e4d8d1aeeaccd885aba3faf31
Mohammad Ali <mscsapan@gmail.com>
2026-01-24 18:38:26 +0600

    ✅ Updated memory management file

--- js/memory_management.js
+++ js/memory_management.js
@@ -1,5 +1,30 @@
+/*
+Call Stack
+! JS Primitive data types
+ ? String, Number, Boolean, undefined, Null, NaN, Sumbol, bigInt
+
+*/
+
+/*
+Heap Stack
+ ? Object, Array
+*/
+
 let myname = 'Mohammad Ali';
 
 myname = 'Ali';
 
-console.log(myname);
+// console.log(myname);
+
+const myInfo = {
+    name: 'Ali',
+    email: 'email@email.com',
+    age: 28
+}
+
+const yourInfo = myInfo;
+
+yourInfo.name = 'Mohammad';
+
+console.log(myInfo);
+console.log(yourInfo);
