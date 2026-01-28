0c92ea8fbcc5306c42da559842f0b8b2a00b66b6
Mohammad Ali <mscsapan@gmail.com>
2026-01-28 20:27:46 +0600

    📄 JS promise file added for learning

--- advance/promise.js
+++ advance/promise.js
@@ -1,1 +1,8 @@
-const promise = new Promise();
+// const promise = new Promise();
+
+setTimeout(() => {
+    console.log('This is from timeout function');
+}, 1000);
+
+console.log('Out of timeout func');
+
